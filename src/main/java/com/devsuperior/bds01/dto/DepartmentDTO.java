45e1c9a1ff9a37e9d1c12c31e140d909ceac806e
Paulo Costa <pjrc83@gmail.com>
2022-11-11 16:38:07 -0300

    Tests passing

---
+++ src/main/java/com/devsuperior/bds01/dto/DepartmentDTO.java
@@ -0,0 +1,46 @@
+package com.devsuperior.bds01.dto;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.devsuperior.bds01.entities.Department;
+import com.devsuperior.bds01.entities.Employee;
+
+public class DepartmentDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	public Long id;
+	public String name;
+
+	public List<Employee> employees = new ArrayList<>();
+
+	public DepartmentDTO() {
+	}
+
+	public DepartmentDTO(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+	public DepartmentDTO(Department entity) {
+		id = entity.getId();
+		name = entity.getName();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
